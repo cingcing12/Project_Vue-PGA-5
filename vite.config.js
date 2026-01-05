bc610936d45d25eba0c609d00c88a6b44abbdebf
Vong sokpheak <vongsokpheak4@gmail.com>
2026-01-05 22:00:37 +0700

    Fix vite config base path

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // REPLACE 'repo-name' WITH YOUR ACTUAL GITHUB REPOSITORY NAME
-  base: '/CAF-X/', 
+  
+  // 👇 CHANGE THIS LINE EXACTLY TO THIS:
+  base: './', 
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
